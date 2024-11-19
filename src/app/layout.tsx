d5e0304654a8b8ef81ea87fe4a8aee15d8bc146a
Catstein - Lucas de Oliveira <lucasdeoliveira@pecege.com>
2024-11-19 19:13:24 -0300

    chore: commit with WIP components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import { Navbar } from "@/components/Navbar";
+import { Toast } from "@/components/Toast";
+import { TokenProvider } from "@/contexts/useToken";
+import { inter } from "@/lib/fonts/fonts";
 import type { Metadata } from "next";
 import "./globals.css";
-import { inter } from "./fonts/fonts";
 
 export const metadata: Metadata = {
   title: "Conselho Tutelar de Piracicaba",
@@ -15,9 +18,12 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`${inter.className} flex items-center justify-center  antialiased w-screen h-screen bg-[#F4F4F5]`}
+        className={`${inter.className} flex flex-col items-center justify-center subpixel-antialiased w-screen h-screen bg-[#F4F4F5]`}
       >
-        {children}
+        <TokenProvider>
+          <Navbar />
+          <Toast>{children}</Toast>
+        </TokenProvider>
       </body>
     </html>
   );
--- src/app/login/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Card } from "./_lib/components/Card";
-
-export default function LoginPage() {
-  return <div className="flex h-min">
-    <Card />
-  </div>;
-}
